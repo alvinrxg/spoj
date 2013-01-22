552423969eaff8b086ca886859e9d7bf08c8a5a8
alvinrxg <alvin.rxg@gmail.com>
2013-01-22 14:02:56 +0100

    update

--- p206.c
+++ p206.c
@@ -1,105 +1,91 @@
 /* http://www.spoj.pl/problems/BITMAP/ */
-/*		TLE		*/
 
+/* bfs...
+ * already better, but still TLE... */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
-void calc(char matrix[][183], int new[][183], int n, int m)
+int n, m;
+char bitmap[182][183];
+int distance[182][182];
+
+
+void calc_dist(int x, int y)
+{
+	int l;
+	int i, j;
+	int f = 1;
+
+	if (y == 0 && x) {
+		l = distance[x - 1][0] - 1;
+	}
+	else if (y) {
+		l = distance[x][y - 1] - 1;
+	}
+	else {
+		l = 1;
+	}
+	do {
+		for (i = 0; i <= l; i++) {
+			j = l - i;
+			// 4 directions
+			if (	   (i + x < n  && j + y < m  && bitmap[x + i][y + j] == '1')
+				|| (i + x < n  && y - j >= 0 && bitmap[x + i][y - j] == '1')
+				|| (x - i >= 0 && j + y < m  && bitmap[x - i][y + j] == '1')
+				|| (x - i >= 0 && y - j >= 0 && bitmap[x - i][y - j] == '1'))
+			{
+				distance[x][y] = l;
+				f = 0;
+				break;
+			}
+		}
+
+	} while (f && ++l);
+}
+
+void calc()
 {
 	int i, j;
-	int x, y, k;
-	int len;
 
-	i = 0;
-	while (i < n) {
-		j = 0;
-		while (j < m) {
-			if (matrix[i][j] == '1') {
-				new[i][j] = 0;
-			}
-			else {
-				k = 0;
-				while(++k) {
-					new[i][j] = 0;
-					y = 0;
-					while (y <= k) {
-						x = k - y;
-						if (i + x < n) {
-							if (j + y < m) {
-								if (matrix[i + x][j + y] == '1') {
-									new[i][j] = k;
-									break;
-								}
-							}
-							if (j - y >= 0) {
-								if (matrix[i + x][j - y] == '1') {
-									new[i][j] = k;
-									break;
-								}
-							}
-						}
-						if(new[i][j] == 0) {
-							if (i - x >= 0) {
-								if (j + y < m) {
-									if (matrix[i - x][j + y] == '1') {
-										new[i][j] = k;
-										break;
-									}
-								}
-								if (j - y >= 0) {
-									if (matrix[i - x][j - y] == '1') {
-										new[i][j] = k;
-										break;
-									}
-								}
-							}
-						}
-
-						y++;
-					}
-					if (new[i][j])
-						break;
-				}
-			}
-			j++;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			if (bitmap[i][j] == '1')
+				distance[i][j] = 0;
+			else
+				calc_dist(i, j);
 		}
-		i++;
 	}
 }
 
-void prints(int new[][183], int n, int m)
+void prints()
 {
 	int i, j;
-	i = 0;
-	while (i < n) {
-		j = 0;
-		while (j < m - 1) {
-			printf("%d ", new[i][j]);
-			j++;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < m; j++) {
+			if (j == 0) {
+				printf("%d", distance[i][j]);
+			}
+			else
+				printf(" %d", distance[i][j]);
 		}
-		printf("%d\n", new[i][j]);
-		i++;
+		printf("\n");
 	}
 }
+
 int main()
 {
 	int t;
-	int n, m;
-	char matrix[182][183];
-	int new[182][183];
-	int i, j;
+	int i;
 
 	scanf("%d", &t);
-	while (t--) {
+	while(t--) {
 		scanf("%d %d", &n, &m);
-		i = 0;
-		while (i < n) {
-			scanf("%s", matrix[i]);
-			i++;
-		}
-
-		calc(matrix, new, n, m);
-		prints(new, n, m);
+		for (i = 0; i < n; i++)
+			scanf("%s", bitmap[i]);
+		calc();
+		prints();
 	}
 
 	return 0;
--- p78.c
+++ p78.c
@@ -1,49 +1,64 @@
 /* http://www.spoj.pl/problems/MARBLES/ */
-/*	need bigger memory	*/
+/* well, don't understand why must n-1, k-1 (n-k-1+1) ... */
+/* TLE */
 
 #include <stdio.h>
+
+long long kkk[1000001];
+long long calc(long long n, long long k)
+{
+	long long big, small;
+	long long rt;
+	long long t, n1, k1;
+	long long i;
+
+	rt = 1;
+	t = n - k;
+	n1 = n - 1;
+	k1 = k - 1;
+
+	big = t > k1 ? k1 : t;
+	small = t < k1 ? k1 : t;
+
+	while(n1 > big) {
+		for (i = small; i > 1; i--) {
+			if (kkk[i] == 0 && n1 % i == 0) {
+				kkk[i] = 1;
+				break;
+			}
+		}
+		if (i == 1) {
+			rt *= n1;
+		}
+		else {
+			rt *= (n1 / i);
+		}
+		n1--;
+	}
+	for (i = small; i > 1; i--) {
+		if (kkk[i] == 0) {
+			rt /= i;
+		}
+		else {
+			kkk[i] = 0;
+		}
+	}
+	return rt;
+}
 
 int main()
 {
 	int t;
-	long n, k;
-	long long x;
-	int jj;
-	long i, j;
-	long k_min, k_max;
+	long long n, k;
+
+	for (n = 0; n < 1000001; n++) {
+		kkk[n] = 0;
+	}
 
 	scanf("%d", &t);
-	jj = 0;
-	while (jj++ < t) {
-		scanf("%ld %ld", &n, &k);
-
-		if (n == k) {
-			printf("1\n");
-			continue;
-		}
-
-		n--;
-		k--;
-
-		k_max = (n - k) > k ? n - k : k;
-		k_min = n - k_max;
-		x = 1;
-		j = 2;
-		for (i = k_max + 1; i <= n; i++) {
-			if (i % j == 0 && j <= k_min) {
-				x *= i / j;
-				j++;
-			}
-			else {
-				x *= i;
-			}
-printf("i = %ld\tj = %ld\tk_max = %ld\tk_min = %ld\tn = %ld\tx = %lld\n", i, j, k_max, k_min, n, x);
-		}
-		for (; j <= k_min; j++) {
-			x /= j;
-printf("j = %ld\tx = %lld\n", j, x);
-		}
-		printf("%lld\n", x);
+	while(t--) {
+		scanf("%lld %lld", &n, &k);
+		printf("%lld\n", calc(n, k));
 	}
 	return 0;
 }
